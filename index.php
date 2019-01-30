04df20caf4a4c5a17417db217d49c31a2918eb9d
NyeinChanAungNCA <nyeinchanaung.nk@gmail.com>
2019-01-30 14:42:39 +0630

    Add index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello World</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<h2>Hello World</h2>
+			</div>
+		</div>
+	</div>
+	
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</body>
+</html>
